c95b9cae0691ac4a55e83de908a2977f1c6cbde1
Rosemary Jammal <rjammal@gmail.com>
2014-08-26 17:28:39 -0700

    implemented get, set, and remove, though remove needs some drying out

--- Python/binary_search_tree.py
+++ Python/binary_search_tree.py
@@ -4,34 +4,129 @@ from random import shuffle
 class BinarySearchTree:
 
     class Node:
-        def __init__(self, key, val):
+        def __init__(self, key, value):
             self.key = key
-            self.val = val
+            self.value = value
             self.left = None
             self.right = None
 
     def __init__(self):
         self.root = None
 
-    def set(self, key, val):
-        pass
+    def set(self, key, value):
+        if not self.root:
+            self.root = BinarySearchTree.Node(key, value)
+            return
+
+        current = self.root
+        while True:
+            if key < current.key:
+                if current.left:
+                    current = current.left
+                else:
+                    current.left = BinarySearchTree.Node(key, value)
+                    return
+            elif key > current.key:
+                if current.right:
+                    current = current.right
+                else:
+                    current.right = BinarySearchTree.Node(key, value)
+                    return
+            else: 
+                current.value = value
+                return
+
 
     def get(self, key):
-        pass
+        current = self.root
+
+        while current:
+            if key < current.key:
+                current = current.left
+            elif key > current.key:
+                current = current.right
+            else: 
+                return current.value
+
+        raise KeyError("Key not present.")
 
     def remove(self, key):
+        if not self.root:
+            raise KeyError("Key not present.")
+        if self.root.key == key:
+            if self.root.left == None:
+                self.root = self.root.right
+            elif self.root.right == None:
+                self.root = self.root.left
+            else:
+                parent = self.root
+                current = self.root.left
+                while current.right: 
+                    parent = current
+                    current = current.right
+
+                if parent == self.root:
+                    parent.left = current.left
+                else:
+                    parent.right = current.left
+                current.left = self.root.left
+                current.right = self.root.right
+                self.root = current
+            return
+        
+        parent = self.root
+        current = self.root
+        while current: 
+            if current.key > key:
+                parent = current
+                current = current.left
+                continue
+            elif current.key < key:
+                parent = current
+                current = current.right
+                continue
+
+            if current.right == None: 
+                if parent.left == current:
+                    parent.left = current.left
+                else: 
+                    parent.right = current.left
+                return
+            if current.left == None: 
+                if parent.left == current:
+                    parent.left == current.right
+                else:
+                    parent.right == current.right
+                return
+
+            largest_child_parent = current
+            largest_child = current.left
+            while largest_child.right:
+                largest_child_parent = largest_child
+                largest_child = largest_child.right
+            if largest_child_parent == parent:
+                parent.left = largest_child.left
+            else:
+                largest_child_parent.right = largest_child.left
+
+            current.key = largest_child.key
+            current.value = largest_child.value
+            return
+
+        raise KeyError("Key not present.")
+
+
+
+    def bfs(self, value):
         pass
 
-    def bfs(self, key):
+    def dfs_in_order(self, value):
         pass
 
-    def dfs_in_order(self, key):
+    def dfs_pre_order(self, value):
         pass
 
-    def dfs_pre_order(self, key):
-        pass
-
-    def dfs_post_order(self, key):
+    def dfs_post_order(self, value):
         pass
 
     def find_node(self, key):
